f93719b7b16598e41cd7da0540870089027e9688
Porawit Poboonma <ball6847@gmail.com>
2017-09-21 22:48:02 +0700

    add build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-docker run -it --rm=true -v $PWD:/app -w /app node:8-alpine sh -c "yarn && yarn run ng -- build -prod"
-docker build -t 192.168.100.254:5000/frontend-war-angular .
+docker run -it --rm=true -v $PWD:/app -w /app node:8-slim sh -c "yarn && yarn run ng build -prod"
+# docker build -t 192.168.100.254:5000/frontend-war-angular .
